b9359b129d24be3dd97dc9f4b2f650e7d0f8f06c
Chege2004 <chegemanasseh2@gmail.com>
2024-07-25 19:47:25 +0300

    Solved the loadpage issue  by adding the first movie and its details

---
+++ workspace/index.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", function () {
+    loadPage();
+});
+
+function loadPage() {
+    fetch("http://localhost:3000/films/1")
+        .then(response => response.json())
+        .then(data => {
+            const posterUrl = document.getElementById("poster");
+            const title = document.getElementById("title");
+            const description = document.getElementById("description");
+            const runtime = document.getElementById("runtime");
+            const showtime = document.getElementById("showtime");
+            const availableTickets = document.getElementById("available-tickets");
+
+            const ticketsAvailable = data.capacity - data.tickets_sold;
+
+            posterUrl.src = data.poster;
+            title.textContent = data.title;
+            description.textContent = data.description;
+            runtime.textContent = data.runtime;
+            showtime.textContent = data.showtime;
+            availableTickets.textContent = `Available Tickets: ${ticketsAvailable}`;
+        })
+        .catch(error => console.error('Error fetching data:', error));
+}
+
